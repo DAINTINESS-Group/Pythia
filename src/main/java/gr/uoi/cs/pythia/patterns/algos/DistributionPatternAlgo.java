79024bd35090a3cf1d80c2b394c5ade54162fd6d
ch-ant <ant.chr@outlook.com>
2022-12-09 21:45:02 +0200

    Add minor fix on DistributionPatternAlgo class
    - Fix identify method on DistributionPatternAlgo class to be compatible with changes on the IpatternAlgo interface. Currently the identify method has an example of usage of the KernelDensity estimate method.

--- src/main/java/gr/uoi/cs/pythia/patterns/algos/DistributionPatternAlgo.java
+++ src/main/java/gr/uoi/cs/pythia/patterns/algos/DistributionPatternAlgo.java
@@ -1,25 +1,46 @@
 package gr.uoi.cs.pythia.patterns.algos;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.stat.KernelDensity;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+
+import gr.uoi.cs.pythia.model.DatasetProfile;
 
 public class DistributionPatternAlgo implements IPatternAlgo {
 
 	@Override
-	public void identify(List<Row> measurementColumn, List<Row> coordinateColumn) {
+	public void identify(Dataset<Row> dataset, DatasetProfile datasetProfile) {
 		// TODO Actually check for a distribution highlight
-		// Currently this method just iterates through the values of the two columns and prints them
-		String str = "Identifying potential distribution pattern highlights...";
-		str += "\nMeasurement column values: ";
-		for (Row measurementValue : measurementColumn) {
-			str += measurementValue.get(0).toString() + " ";
-		}
-		str += "\nCoordinate column values: ";
-		for (Row coordinateValue : coordinateColumn) {
-			str += coordinateValue.get(0).toString() + " ";
-		}
-		System.out.println(str);
+		// Currently this method has an example of usage of the KernelDensity estimate method
+		
+		SparkContext sc =SparkContext.getOrCreate();
+		JavaSparkContext jsc = JavaSparkContext.fromSparkContext(sc);
+		
+		// Just a dummy JavaRDD
+		JavaRDD<Double> data = jsc.parallelize(
+				  Arrays.asList(1.0, 1.0, 1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 6.0, 7.0, 8.0, 9.0, 9.0));
+		
+		List<Double> datasetColumn = dataset.select("download")
+				.collectAsList().stream()
+				.map(s -> Double.parseDouble(s.get(0).toString()))
+				.collect(Collectors.toList());
+		
+		JavaRDD<Double> datasetColumnRDD = jsc.parallelize(datasetColumn);
+		// Construct the density estimator with the sample data
+		// and a standard deviation for the Gaussian kernels
+		KernelDensity kd = new KernelDensity().setSample(datasetColumnRDD).setBandwidth(3.0);
+
+		// Find density estimates for the given values
+		double[] densities = kd.estimate(new double[]{-1.0, 2.0, 5.0});
+
+		System.out.println(Arrays.toString(densities));
 	}
 
 }
--- src/main/java/gr/uoi/cs/pythia/patterns/algos/IPatternAlgo.java
+++ src/main/java/gr/uoi/cs/pythia/patterns/algos/IPatternAlgo.java
@@ -8,6 +8,5 @@ import org.apache.spark.sql.Row;
 import gr.uoi.cs.pythia.model.DatasetProfile;
 
 public interface IPatternAlgo {
-//	void identify(List<Row> measurementColumn, List<Row> coordinateColumn);
 	void identify(Dataset<Row> dataset, DatasetProfile datasetProfile) throws IOException;
 }
