6c8393fd47fcd64cd0255923e5bc4dafd8ab1864
lamprosvlax13 <vlahopoulos_lampros@hotmail.com>
2024-05-08 19:49:06 +0300

    fix junit +add filesize,path,timestamp in testProduceReportTxt

--- src/main/java/gr/uoi/cs/pythia/cardinalities/CardinalitiesCalculatorFactory.java
+++ src/main/java/gr/uoi/cs/pythia/cardinalities/CardinalitiesCalculatorFactory.java
@@ -11,7 +11,7 @@ public class CardinalitiesCalculatorFactory {
     public ICardinalitiesCalculator createCardinalitiesCalculator(Dataset<Row> dataset,String columnName){
         cardinalitiesCalculator= new SparkCardinalitiesCalculator(dataset,columnName);
         executeCalculations();
-        return new SparkCardinalitiesCalculator(dataset,columnName);
+        return cardinalitiesCalculator;
     }
 
 
