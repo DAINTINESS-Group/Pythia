11b64d5a3fc164c96bcf2e7e54d2dffdd82c8b7a
lamprosvlax13 <vlahopoulos_lampros@hotmail.com>
2024-05-08 19:25:11 +0300

    CardinalitiesCalculation(Null,distinct) ++JunitTests ++EditCardinalitiesFactory

--- src/main/java/gr/uoi/cs/pythia/cardinalities/CardinalitiesCalculatorFactory.java
+++ src/main/java/gr/uoi/cs/pythia/cardinalities/CardinalitiesCalculatorFactory.java
@@ -4,8 +4,20 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 public class CardinalitiesCalculatorFactory {
-    
+
+
+    private ICardinalitiesCalculator cardinalitiesCalculator;
+
     public ICardinalitiesCalculator createCardinalitiesCalculator(Dataset<Row> dataset,String columnName){
+        cardinalitiesCalculator= new SparkCardinalitiesCalculator(dataset,columnName);
+        executeCalculations();
         return new SparkCardinalitiesCalculator(dataset,columnName);
     }
+
+
+    private void executeCalculations(){
+        cardinalitiesCalculator.calculateNumberOfNullValues();
+        cardinalitiesCalculator.calculateDistincValues();
+    }
+
 }
